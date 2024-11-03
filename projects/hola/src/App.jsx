10cd5127a1f2ec11400e812d6a2ea6ac863bd5b4
jorge <ogomez.isc@gmail.com>
2024-11-03 13:00:39 -0600

    Se agrego un estado para el boton de seguir

--- projects/hola/src/App.jsx
+++ projects/hola/src/App.jsx
@@ -6,7 +6,7 @@ export function App() {
 
     const format = (userName) => `@${userName}`
 
-    const landogomez = {isFollowing: false, userName: 'landogomez', formatUserName: format} //Pasar objeto como props
+    const landogomez = { userName: 'landogomez', formatUserName: format} //Pasar objeto como props
     // Aunque muchas veces es mejor ser declarativo y pasar las props directamente
 
     return (
@@ -19,10 +19,10 @@ export function App() {
         <FollowCard //Pasar props directamente
             formatUserName={format} 
             userName="vastidefreitas" 
-            isFollowing >
+             >
               V
           </FollowCard>
-        <FollowCard formatUserName={format}  userName="Ravens" isFollowing={false}
+        <FollowCard formatUserName={format}  userName="Ravens" 
         />
       </div>
       </>
--- projects/hola/src/FollowCard.jsx
+++ projects/hola/src/FollowCard.jsx
@@ -1,7 +1,19 @@
-export function FollowCard({ children ,formatUserName ,userName = 'unknown'/*Valor por defecto*/ ,  isFollowing }){
+import { useState } from "react"
+// useState es un Hook que permite añadir estado a los componentes funcionales
 
+export function FollowCard({ children ,formatUserName ,userName = 'unknown'/*Valor por defecto*/ }){
+    const [isFollowing, setIsFollowing] = useState(false) //Estado inicial
+    /* La linea de arriba es igual a esto (se le conoce como destructuring) = 
+    const state = useState(false)
+    const isFollowing = state[0]
+    const setIsFollwing = state[1]*/ 
+    
     const text = isFollowing ? 'Siguiendo' : 'Seguir'
     const buttonClass = isFollowing ? 'tw-follow-card-btn following' : 'tw-follow-card-btn'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing) //Cambia el estado
+    }
 
     return (
         <article className='tw-follow-card'>
@@ -15,7 +27,7 @@ export function FollowCard({ children ,formatUserName ,userName = 'unknown'/*Val
         </header>
 
         <aside>
-            <button className={buttonClass}> 
+            <button className={buttonClass} onClick={handleClick}> 
                 {text}  
             </button>
         </aside>
