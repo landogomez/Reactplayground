361e59c6976b1c1444f3d2adae32377dc77a49e8
jorge <ogomez.isc@gmail.com>
2024-11-04 08:07:27 -0600

    Se agrego la actualizacion del estado board

--- projects/gato/02-gato/src/App.jsx
+++ projects/gato/02-gato/src/App.jsx
@@ -9,8 +9,12 @@ const TURNS = {
 const Square = ({ children, updateBoard, isSelected,index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
   
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -20,8 +24,20 @@ function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null)) // Se crea un array de 9 elementos con valor null
 
+  
   const [turn, setTurn] = useState(TURNS.X) // Se crea un estado para el turno
- return (
+
+  const updateBoard = (index) => {
+    const newBoard = [...board] // Se crea una copia del array
+    newBoard[index] = turn // Se actualiza el valor del array en la posicion index
+    setBoard(newBoard) // Se actualiza el estado del array
+    
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X // Se cambia el turno
+    setTurn(newTurn) // Se actualiza el estado del turno
+  }
+ 
+ 
+  return (
   <>
     <main className="board">
       <h1>Tic Tac Toe</h1>
@@ -32,6 +48,7 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
                 >
                   {board[index]} {/* Se le pasa el valor del array */}
                 </Square>
