d238741a68065821a73f8e21ad8f0075e9385ad4
jorge <ogomez.isc@gmail.com>
2024-11-04 07:53:40 -0600

    Muestra el turno del jugador

--- projects/gato/02-gato/src/App.jsx
+++ projects/gato/02-gato/src/App.jsx
@@ -1,37 +1,53 @@
+import { useState } from "react"
+
 const TURNS = {
   X: 'X',
   O: 'O'
 }
 
-const board = Array(9).fill(null)
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, updateBoard, isSelected,index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  
   return (
-    <div className="square">
+    <div className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null)) // Se crea un array de 9 elementos con valor null
+
+  const [turn, setTurn] = useState(TURNS.X) // Se crea un estado para el turno
  return (
   <>
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {
-          board.map((_, index) => {
+          board.map((_, index) => { // map recibe dos argumentos, el primero es el valor del array y el segundo es el indice
             return (
               <Square
                 key={index}
                 index={index}
                 >
-                  
+                  {board[index]} {/* Se le pasa el valor del array */}
                 </Square>
 
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected = {turn === TURNS.X}>
+          {TURNS.X}
+        </Square> 
+        <Square isSelected = {turn === TURNS.O}>
+          {TURNS.O}
+         </Square> 
       </section>
     </main>
   </>
